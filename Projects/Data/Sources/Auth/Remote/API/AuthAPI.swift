f883de982b2d8347792404151c1e5fe9e6a2b050
parkdoyeon1 <parkdoyeon3239@gmail.com>
2024-05-05 20:24:03 +0900

    [ADD] AuthAPI

---
+++ Projects/Data/Sources/Auth/Remote/API/AuthAPI.swift
@@ -0,0 +1,81 @@
+import Moya
+import Foundation
+import Core
+
+public enum AuthAPI {
+    case login(
+        email: String,
+        password: String
+    )
+    case sendEmail(email: String)
+    case emailCheck(
+        email: String,
+        code: Int
+    )
+    case signup(
+        email: String,
+        password: String,
+        nickName: String,
+        genre: String
+    )
+}
+
+extension AuthAPI: TargetType {
+    public var baseURL: URL {
+        return BaseURL.baseURL
+    }
+    
+    public var path: String {
+        switch self {
+        case .login:
+            return "/users/login"
+        case .sendEmail:
+            return "/users/email"
+        case .emailCheck:
+            return "/users/email/check"
+        case .signup:
+            return "/users/signup"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .login, .sendEmail, .emailCheck, .signup:
+            return .post
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        case .login(let email, let password):
+            return .requestParameters(
+                parameters: [
+                    "email": email,
+                    "password": password
+                ], encoding: JSONEncoding.default)
+        case .sendEmail(let email):
+            return .requestParameters(
+                parameters: [
+                    "email": email
+                ], encoding: JSONEncoding.default)
+        case .emailCheck(let email, let code):
+            return .requestParameters(
+                parameters: [
+                    "email": email,
+                    "code": code
+                ], encoding: JSONEncoding.default)
+        case .signup(let email, let password, let nickName, let genre):
+            return .requestParameters(
+                parameters: [
+                    "email": email,
+                    "password": password,
+                    "nick_name": nickName,
+                    "genre": genre
+                ], encoding: JSONEncoding.default)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        return .none
+    }
+}
