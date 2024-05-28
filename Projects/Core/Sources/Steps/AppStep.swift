d1ecd33729cde25d5361a8389c8aaa436346059a
parkdoyeon1 <parkdoyeon3239@gmail.com>
2024-05-28 15:09:19 +0900

    [UPDATE] Steps

--- Projects/Core/Sources/Steps/AppStep.swift
+++ Projects/Core/Sources/Steps/AppStep.swift
@@ -8,11 +8,18 @@ public enum AppStep: Step {
     case emailInputIsRequired
     case emailCheckIsRequired(email: String)
     case signupIsRequired(email: String)
+    case profileIsRequired
+    case profileEditRequired
     case successSignup
     case homeIsRequired
     case searchIsRequired
     case bookReviewIsRequired(isbn: String)
     case bookReviewWriteIsRequired(isbn: String)
+    case bookReviewDetailIsRequired(reviewId: String)
+    case bookReviewEditIsRequired(isbn: String)
+    case bookReviewDeleteIsRequired(isbn: String)
+
+    case popIsRequird
 
     case testIsRequired
     case error
